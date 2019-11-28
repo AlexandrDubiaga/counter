31f213f65d585d363ad128d554d154f01018510c
Alex <alexandr.dubiaga@gmail.com>
2019-11-28 16:33:14 +0200

    Updates

---
+++ precache-manifest.a01c9998f8001f5856c553f105687001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd43a39f7798f14379cbe8ab2372381",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "f34c708435bcb30cb021",
+    "url": "/counter/static/css/main.bfee475a.chunk.css"
+  },
+  {
+    "revision": "7c34936ec5c811de465a",
+    "url": "/counter/static/js/2.631d3757.chunk.js"
+  },
+  {
+    "revision": "f34c708435bcb30cb021",
+    "url": "/counter/static/js/main.590ae327.chunk.js"
+  },
+  {
+    "revision": "67b5cf53f1d639f4a016",
+    "url": "/counter/static/js/runtime-main.4a936d9b.js"
+  }
+]);
